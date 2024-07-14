0eb8f92ed21ad879e58e6dc92f7be39471c1a0bb
Harlok <holmik90@yandex.ru>
2024-07-14 23:57:19 +0400

    chore: remove extra files

--- PGS.TemplatePlaceholderBot/IWordReader.cs
+++
@@ -1,7 +0,0 @@
-namespace TelegramBot;
-
-public interface IWordReader
-{
-    List<string> GetKeywords(string filePath);
-    Guid FillWord(string templatePath, Dictionary<string, string> keywords);
-}
