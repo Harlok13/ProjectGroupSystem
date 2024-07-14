684ff8468e0b306a356afbbc56c8e55cc2df9048
Harlok <holmik90@yandex.ru>
2024-07-14 23:56:02 +0400

    feat: add EBotTrigger

---
+++ PGS.TemplatePlaceholderBot/States/EBotTrigger.cs
@@ -0,0 +1,9 @@
+namespace PGS.TemplatePlaceholderBot.States;
+
+public enum EBotTrigger : byte
+{
+    EnterTemplateNumberForDelete,
+    EnterTemplateNumberForChoice,
+    EnterTemplateForCreateExcel,
+    Cancel
+}
