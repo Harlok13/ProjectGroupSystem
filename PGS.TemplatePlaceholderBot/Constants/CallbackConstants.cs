40ad904e13edaae2ba4f81c9f515ccefa1e249ee
Harlok <holmik90@yandex.ru>
2024-07-14 23:56:30 +0400

    feat: add CallbackConstants

---
+++ PGS.TemplatePlaceholderBot/Constants/CallbackConstants.cs
@@ -0,0 +1,15 @@
+namespace PGS.TemplatePlaceholderBot.Constants;
+
+public static class CallbackConstants
+{
+    public const string GetTemplates = "MainMenu";
+    
+    public const string RemoveTemplate = "RemoveTemplate";
+    public const string ChoiceTemplate = "ChoiceTemplate";
+    
+    public const string ChoiceHowCreateExcelByTemplate = "ChoiceHowCreateExcelByTemplate";
+    public const string UseCurrentTemplateForGeneratingExcel = "UseCurrentTemplateForGeneratingExcel";
+    public const string DownloadNewTemplateForGeneratingExcel = "DownloadNewTemplateForGeneratingExcel";
+    
+    public const string Cancel = "Cancel";
+}
